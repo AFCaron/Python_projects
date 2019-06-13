0e6564f7f402f1688938e2904c56de4f4e9926f9
ACaron <43013348+Bidartarra@users.noreply.github.com>
2019-06-12 20:55:44 -0600

    Create Product_inventory_project.py

---
+++ Product_inventory_project.py
@@ -0,0 +1,32 @@
+#Product Inventory Project
+#Create an application which manages an inventory of products.
+#Create a product class which has a price, id, and quantity on hand.
+#Then create an inventory class which keeps track of various products and can sum up the inventory value.
+
+
+#product class
+class Product(self, price, idn, quantity):
+    def__init__(self):
+        self.price = price
+        self.idn = idn
+        self.quantity = quantity
+        
+        
+
+        
+#create a loop to keep the program running
+prog = 'run'
+while prog = 'run':
+    #ask what the user wants to do
+    prog = input('what would you like to do? add (a) / remove (r) / sum inventory (s) / quit (q)')
+    if prog = 'a':
+        #routine for adding a product
+            #generating unique ID number each time a product is added
+    elif prog = 'r':
+        #routine for removing a product
+            #selecting the product thru its ID_number
+    elif prog = 's':
+        #routine for sum of the inventory
+    elif prog = 'q':
+        #quitting the program
+        break
