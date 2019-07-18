19d29ab0f70225c6de44824585bdbb3ef0d4378e
Antoine <43013348+AFCaron@users.noreply.github.com>
2019-07-17 19:45:25 -0600

    Update strongPasswordChecks.py

--- strongPasswordChecks.py
+++ strongPasswordChecks.py
@@ -1,8 +1,7 @@
 #strong password checks
+import re
 #ask the user for the password to check
 pwtocheck = input('please enter the password to be checked:')
-#state of assword variable
-pw_state = 'ok'
 
 #first regex : lower case characters
 pw_lc = re.compile(r'''
@@ -18,24 +17,33 @@ pw_sc = re.compile(r'''
         ([\-_\?!@#$%^&\*]+)
         ''', re.VERBOSE)
 
-while pw_state = 'ok':
+
 #is the password at least 8 characters
-    if len(pwtocheck) < 8:
-        print('password is unsecure : too short')
-        break
+if len(pwtocheck) < 8:
+    print('password is too short')
+    #pw_state = 'nok'
+        #break
         
 #first regex is none
-   if pw_lc.search(pwtocheck) is None:
-        print("password doesn't have at least one lower case character")
-        break
+if pw_lc.search(pwtocheck) is None:
+    print("password doesn't have at least one lower case character")
+    #pw_state = 'nok'
+        #break
     
-#second regex : upper case characters
-    elif pw_uc.search(pwtocheck) is None:
-        print("password doesn't have at least one upper case character")
-        break
-#third regex: special characters
-    elif pw_sc.search(pwtocheck) is None:
-        print("password doesn't have at least one special character")
-        break
+#second regex is None
+if pw_uc.search(pwtocheck) is None:
+    print("password doesn't have at least one upper case character")
+    #pw_state = 'nok'
+        #break
+#third regex is None
+if pw_sc.search(pwtocheck) is None:
+    print("password doesn't have at least one special character")
+    #pw_state = 'nok'
+        #break
         
-#to be continued
+else:
+    print('password is ok')
+
+        
+
+print('\nchecks done')
