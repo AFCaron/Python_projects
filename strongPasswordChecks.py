f19b9194f9e1009d0070d27d32b249b7b31f046d
Antoine <43013348+AFCaron@users.noreply.github.com>
2019-07-19 20:40:34 -0600

    Update strongPasswordChecks.py

--- strongPasswordChecks.py
+++ strongPasswordChecks.py
@@ -1,49 +1,54 @@
 #strong password checks
 import re
+
 #ask the user for the password to check
 pwtocheck = input('please enter the password to be checked:')
 
-#first regex : lower case characters
-pw_lc = re.compile(r'''
-    ([a-z]+)
-    ''', re.VERBOSE)
+#Error message to be
+Err_mess = ''
+
+#first regex : at least one lower case characters
+pw_lc = re.compile(r'([a-z]+)')
+        
+#second regex : at least one upper case characters
+pw_uc = re.compile(r'([A-Z]+)')
     
-#second regex : upper case characters
-pw_uc = re.compile(r'''
-    ([A-Z]+)
-    ''', re.VERBOSE)
-#third regex: special characters
-pw_sc = re.compile(r'''
-        ([\-_\?!@#$%^&\*]+)
-        ''', re.VERBOSE)
+#third regex: at least one special character
+pw_sc = re.compile(r'([\-_\?!@#$%^&\*]+)')
+
+#fourth regex : password must have at least one number
+pw_d = re.compile(r'([0-9])+')
+
+#create a list to be given to all() to evaluate if all conditions are met or not
+cond_pw = [len(pwtocheck) > 8, pw_lc.search(pwtocheck), pw_uc.search(pwtocheck), pw_sc.search(pwtocheck), pw_d.search(pwtocheck)]
 
 
+#building an error message
 #is the password at least 8 characters
 if len(pwtocheck) < 8:
-    print('password is too short')
-    #pw_state = 'nok'
-        #break
-        
+    Err_mess = Err_mess + 'password is too short\n'
+    
 #first regex is none
 if pw_lc.search(pwtocheck) is None:
-    print("password doesn't have at least one lower case character")
-    #pw_state = 'nok'
-        #break
+    Err_mess = Err_mess + 'password needs at least one lower case character\n'
     
 #second regex is None
 if pw_uc.search(pwtocheck) is None:
-    print("password doesn't have at least one upper case character")
-    #pw_state = 'nok'
-        #break
+    Err_mess = Err_mess + 'password needs at least one upper case character\n'
+
+
 #third regex is None
 if pw_sc.search(pwtocheck) is None:
-    print("password doesn't have at least one special character")
-    #pw_state = 'nok'
-        #break
-        
-else:
+    Err_mess = Err_mess + 'password needs at least one special character\n'
+
+    
+#fourth regex is none
+if pw_d.search(pwtocheck) is None:
+    Err_mess = Err_mess + 'password needs at least one number\n'
+      
+
+if all (cond_pw) is True: 
     print('password is ok')
+else:
+    print(Err_mess)
 
-        
-
-print('\nchecks done')
